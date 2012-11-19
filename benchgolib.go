ba1462f1109c6a84b48a392262ee9f3826229b30
Sasha Crofter <sasha@crofter.org>
2012-11-19 16:13:36 -0500

    added RecieveMessage() and a SID field to Message
    I added a new function, RecieveMessage(), for retrieving a Message type
    directly from the connection.
    
    I also added a SID field to the Message type, as well as a means of
    initialization in both the Message and Session types.

--- benchgolib.go
+++ benchgolib.go
@@ -2,6 +2,7 @@ package benchgolib
 
 import (
 	"github.com/zeebo/bencode"
+	"io"
 	"net"
 	"time"
 )
@@ -14,35 +15,44 @@ const (
 
 //Session is the type which encapsulates a single benchgo session, including temporary key, remote taret, and message history. It can be used to send and recieve message objects.
 type Session struct {
-	ID      string      //Identifier for the session.
+	SID     int64       //Identifier for the session.
 	Key     interface{} //To-be-determined.
 	Remote  string      //The address of the remote participant.
 	History []*Message  //The entire history of message objects.
 }
 
-//The Message type is used to encapsulate lone messages to be sent across the wire. It contains the timestamp and the contents of the message.
+//The Message type is used to encapsulate lone messages, which can be transmitted directly across the wire. It contains the Session ID, timestamp, and the contents of the message, but the timestamp is not transmitted.
 type Message struct {
-	Timestamp string //The time of composition.
-	Content   string //The message contained by the structure.
+	SID       int64     `bencode:"sid"` //The session identifier.
+	Timestamp time.Time `bencode:"-"`   //The time of composition.
+	Content   string    `bencode:"c"`   //The message contained by the structure.
 }
 
 //NewSession initializes a new session with the remote address. The local address or domain is used to identify the initializing client, (such as with a domain name, as opposed to an IP address.
 func NewSession(local, remote string) (s *Session, err error) {
+	//SID should be further randomized, as by
+	//a multiplication or addition, followed
+	//by a hash function.
+	SID := time.Now().UnixNano()
 	s = &Session{
+		SID:     SID,
 		Remote:  remote,
 		History: make([]*Message, 0),
 	}
 	return s, nil
 }
 
-//SendMessage completely encapsulates the process of sending a single message to a single target using a single communication session.
-func (s *Session) SendMessage(m *Message) (err error) {
+//SendMessage completely encapsulates the process of sending a single Message to the remote target using a single communication session. It ensures that the Message's SID field is set to the Session's.
+func (s *Session) SendMessage(m Message) (err error) {
 	conn, err := net.Dial("tcp", s.Remote+":"+port)
 	if err != nil {
 		//If the connection could not be made,
 		//return the error.
 		return
 	}
+	//Stamp the Message with the current SID.
+	m.SID = s.SID
+
 	//Encode direct to the wire.
 	err = bencode.NewEncoder(conn).Encode(m)
 	if err != nil {
@@ -52,14 +62,24 @@ func (s *Session) SendMessage(m *Message) (err error) {
 	}
 	//If sending succeeded, then add the message
 	//to the history.
-	s.History = append(s.History, m)
+	s.History = append(s.History, &m)
+	return
+}
+
+//RecieveMessage is used to retrieve a Message from an input device. It uses bencode to recieve directly from the wire. It does not perform any decryption step.
+func RecieveMessage(r io.Reader) (m *Message, err error) {
+	err = bencode.NewDecoder(r).Decode(&m)
+	//Since we cannot sensibly handle the error
+	//here, we must return it whether or not it
+	//is nil. The Message, whether or not it
+	//came through, will go with it.
 	return
 }
 
 //SendString is a wrapper for SendMessage. It creates a Message with the supplied string in the Content field, and with the timestamp as supplied by time.Now().
 func (s *Session) SendString(content string) error {
-	return s.SendMessage(&Message{
-		Timestamp: time.Now().String(),
+	return s.SendMessage(Message{
+		Timestamp: time.Now(),
 		Content:   content,
 	})
 }
