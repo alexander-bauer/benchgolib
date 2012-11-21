bf764284b843ebb34e2869d23e89fd6f5add25f6
Sasha Crofter <sasha@crofter.org>
2012-11-20 20:28:16 -0500

    added a bunch of useful RSA functions
    I added some encrypt/decrypt RSA functions, which are just calls to
    other functions. I also added a function for generating a session key,
    which is a []byte of length 8, using crypto/rand.
    
    ref #2

--- crypto.go
+++ crypto.go
@@ -3,15 +3,29 @@ package benchgolib
 import (
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha256"
 )
 
+//sessionKeyGen uses crypto/rand to generate 64 random bits to use as a session key.
+func sessionKeyGen() (key []byte, err error) {
+	key = make([]byte, 8)    //Length 64 bits
+	_, err := rand.Read(key) //Fill key with random data
+	return
+}
+
+//rsaGen uses crypto/rsa and crypto/rand to generate a new *rsa.PrivateKey of the given size.
 func rsaGen(size int) (key *rsa.PrivateKey, err error) {
 	return rsa.GenerateKey(rand.Reader, size)
 }
 
-//rsaEncrypt encrypts a byte array to the rsa public key passed it, and returns the resultant byte array.
+//rsaEncrypt encrypts a byte array to the rsa public key passed it, and returns the resultant byte array. It uses a sha256.New() and rand.Reader to pass to rsa.EncryptOAEP.
 func rsaEncrypt(key *rsa.PublicKey, data []byte) (crp []byte, err error) {
-	return nil, nil
+	return rsa.EncryptOAEP(sha256.New(), rand.Reader, key, data)
+}
+
+//rsaDecrypt is the sister function to rsaEncrypt. It .
+func rsaEncrypt(key *rsa.PublicKey, data []byte) (crp []byte, err error) {
+	return rsa.DecryptOAEP(sha256.New(), rand.Reader, key, data)
 }
 
 //arbitraryEncrypt implements a simple for-loop and zero-padding to user s.Cipher to encrypt an arbitrary length byte array. It returns the enciphered byte array, which may not be the same length.
